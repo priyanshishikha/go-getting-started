c85863bbe2f88c6881b41942bf83462a569eb737
Priyanshi <priyanshi.shikha@kartbites.com>
2019-07-22 16:48:34 +0530

    m

--- main.go
+++ main.go
@@ -9,10 +9,17 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	r := gin.Default()
+
+	r.GET("/todo", addTodoItem)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	r.Run(":" + port) // listen and serve on 0.0.0.0:8080
+}
+
+func addTodoItem(c *gin.Context) {
+
+	c.JSON(200, "abc")
 }
